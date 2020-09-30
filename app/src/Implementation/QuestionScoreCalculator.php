866b7096b211ab1ace4916e69f6486f999e8911f
Hijlco Lankhorst <hijlco@declaree.com>
2020-09-30 14:17:38 +0200

    Implement P-Value calculation

--- app/src/Implementation/QuestionScoreCalculator.php
+++ app/src/Implementation/QuestionScoreCalculator.php
@@ -4,13 +4,70 @@
 namespace App\Implementation;
 
 
+use PhpOffice\PhpSpreadsheet\Worksheet\Column;
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
 class QuestionScoreCalculator
 {
 
+    /**
+     * @var int
+     */
+    private $highestRow;
+
     public function calculateQuestionScores(Worksheet $file)
     {
+        $this->highestRow = $file->getHighestDataRow() - 2;
+
+        foreach ($file->getColumnIterator() as $column) {
+            if ($column->getColumnIndex() === "A") {
+                continue;
+            }
+
+            $this->getScoresPerQuestion($column);
+
+
+        }
+        die();
+    }
+
+    private function getScoresPerQuestion(Column $column)
+    {
+        $question = null;
+        foreach ($column->getCellIterator() as $cell) {
+            if ($cell->getRow() === 1) {
+                $question = $cell->getValue();
+                break;
+            }
+        }
+        dump($question);
+        $pValue = $this->calculatePValue($column);
+        dump($pValue);
+    }
+
+    private function calculatePValue(Column $column)
+    {
+        $maxScore = 0;
+        $studentResults = 0;
+        foreach ($column->getCellIterator() as $cell) {
+            if ($cell->getRow() === 1) {
+                continue;
+            }
+
+            if ($cell->getRow() === 2) {
+                $maxScore = $cell->getValue();
+                continue;
+            }
+
+            $studentResults += $cell->getValue();
+        }
+
+        $maxResult = ($maxScore * $this->highestRow);
+        $averageScore = ($studentResults / $this->highestRow);
+        $pValue = ($averageScore / $maxResult);
+
+        return $pValue;
+
 
     }
 }
